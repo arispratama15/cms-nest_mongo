e64f6a519b9108aa5633c4084cb03ee3a3d89601
Aris Pratama <arispratama15@gmail.com>
2021-06-09 20:35:56 +0700

    update e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, HttpStatus } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 import { UsersModule } from '../src/users/users.module';
 import { AuthModule } from '../src/auth/auth.module';
 import { ContentsModule } from '../src/contents/contents.module';
+import * as request from 'supertest';
+import { AppController } from './../src/app.controller';
+import { AppService } from './../src/app.service';
+import { CreateUserDto, UpdateUserDto, GetOneItemDto, DeleteItem } from '../src/users/dto/user.dto';
+import { AuthLoginDto } from '../src/auth/dto/auth-login.dto';
+import { CreateContentDto, UpdateContentDto } from '../src/contents/dto/content.dto';
 
-jest.mock('@nestjs/jwt');
+// jest.mock('../src/users/users.module')
 jest.mock('../src/users/user.entity')
+jest.mock('@nestjs/jwt');
 jest.mock('../src/contents/content.entity')
+// jest.mock('../src/auth/auth.module')
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule, UsersModule, AuthModule, ContentsModule],
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [UsersModule, AuthModule, ContentsModule],
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -27,11 +35,30 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
   });
-  it("should get all users", () => {
-    return request(app.getHttpServer())
-      .get("/users")
-      .set("Accept", "application/json")
-      .expect(HttpStatus.OK);
-
-  });
+  describe('Users url', () => {
+    it('/users (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/users')
+        .expect(200)
+    });
+  })
+  describe('Auth url', () => {
+    it('/login (GET)', () => {
+      const payload: AuthLoginDto = {
+        username: "admin",
+        password: "welcome@123"
+      };
+      return request(app.getHttpServer())
+        .post('/auth')
+        .send(payload)
+        .expect(201)
+    });
+  })
+  describe('Contents url', () => {
+    it('/contents (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/contents')
+        .expect(200)
+    });
+  })
 });
