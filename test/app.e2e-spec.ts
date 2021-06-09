f78cccba58cee63267d90c02f8d518a99f9f2e3a
Aris Pratama <arispratama15@gmail.com>
2021-06-10 00:53:30 +0700

    add e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,64 +1,127 @@
-import { INestApplication } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import { UsersModule } from '../src/users/users.module';
-import { AuthModule } from '../src/auth/auth.module';
-import { ContentsModule } from '../src/contents/contents.module';
-import * as request from 'supertest';
-import { AppController } from './../src/app.controller';
-import { AppService } from './../src/app.service';
-import { CreateUserDto, UpdateUserDto, GetOneItemDto, DeleteItem } from '../src/users/dto/user.dto';
-import { AuthLoginDto } from '../src/auth/dto/auth-login.dto';
-import { CreateContentDto, UpdateContentDto } from '../src/contents/dto/content.dto';
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from './../src/app.module'
+import { User } from '../src/users/user.entity'
 
-// jest.mock('../src/users/users.module')
 jest.mock('../src/users/user.entity')
-jest.mock('@nestjs/jwt');
-jest.mock('../src/contents/content.entity')
-// jest.mock('../src/auth/auth.module')
+
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: INestApplication
 
   beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [UsersModule, AuthModule, ContentsModule],
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, User],
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = moduleFixture.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init()
+
+    // tip: access the database connection via
+    // const connection = app.get(Connection)
+    // const a = connection.manager
+  })
+
+  afterAll(async () => {
+    await Promise.all([
+      app.close(),
+    ])
+  })
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-  });
+      .expect('Hello World!')
+  })
+
   describe('Users url', () => {
     it('/users (GET)', () => {
       return request(app.getHttpServer())
         .get('/users')
         .expect(200)
-    });
+    })
+    it('/users/profile (POST)', () => {
+      const payload = {
+        id: 7
+      }
+      return request(app.getHttpServer())
+        .post('/users/profile')
+        .send(payload)
+        .expect(201)
+    })
+    it('/users/profile (POST) error bad request', () => {
+      const payload = {
+        id: ""
+      }
+      return request(app.getHttpServer())
+        .post('/users/profile')
+        .send(payload)
+        .expect(400)
+    })
+    it('/users/:id (PUT)', () => {
+      const payload = {
+        id: 7,
+        nama: "something"
+      }
+      return request(app.getHttpServer())
+        .put(`/users/${payload.id}`)
+        .send(payload)
+        .expect(200)
+    })
+    it('/users/:id (PUT) error bad request', () => {
+      const payload = {
+        id: 7,
+        nama: ""
+      }
+      return request(app.getHttpServer())
+        .put(`/users/${payload.id}`)
+        .send(payload)
+        .expect(400)
+    })
+    // it('/users/:id (DELETE)', () => {
+    //   const payload = {
+    //     id: 7,
+    //   }
+    //   return request(app.getHttpServer())
+    //     .put(`/users/${payload.id}`)
+    //     .send(payload)
+    //     .expect(200)
+    // })
+    // it('/users/:id (PUT) error bad request', () => {
+    //   const payload = {
+    //     id: 7,
+    //     nama: ""
+    //   }
+    //   return request(app.getHttpServer())
+    //     .put(`/users/${payload.id}`)
+    //     .send(payload)
+    //     .expect(400)
+    // })
   })
   describe('Auth url', () => {
-    it('/login (GET)', () => {
-      const payload: AuthLoginDto = {
+    it('/auth (Post)', () => {
+      const payload = {
         username: "admin",
         password: "welcome@123"
-      };
+      }
       return request(app.getHttpServer())
         .post('/auth')
         .send(payload)
         .expect(201)
-    });
-  })
-  describe('Contents url', () => {
-    it('/contents (GET)', () => {
+    })
+    it('/auth (Post) error unauthorized', () => {
+      const payload = {
+        username: "admin",
+        password: "lala"
+      }
       return request(app.getHttpServer())
-        .get('/contents')
-        .expect(200)
-    });
+        .post('/auth')
+        .send(payload)
+        .expect(401)
+    })
   })
-});
+
+})
